6300c7ab0765abab4010bfab7030286a45473318
Ritik Singh <singhritik3133@gmail.com>
2026-03-31 00:06:27 +0530

    Add files via upload

---
+++ NumberNode.java
@@ -0,0 +1,12 @@
+public class NumberNode implements Expression {
+    private final double value;
+
+    public NumberNode(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public Object evaluate(Environment env) {
+        return value;
+    }
+}
